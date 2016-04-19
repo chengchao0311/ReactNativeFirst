71685969cf3525dfcc73933ace4cdad61570d5d1
Shawn Chan <chengchao0311@gmail.com>
2016-04-20 06:59:15 +0800

    Before Fetching Real Data

--- index.ios.js
+++ index.ios.js
@@ -1,30 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
 import React, {
   AppRegistry,
   Component,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
 
 class AwesomeProject extends Component {
   render() {
+    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-        Hello World!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image 
+           source={{uri: movie.posters.thumbnail}} 
+           style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+           <Text>{movie.title}</Text>
+           <Text>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -33,20 +32,28 @@ class AwesomeProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  thumbnail: {
+     width: 53,
+     height: 81,
+     marginLeft: 5,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  rightContainer: {
+    flex: 1,
+    alignItems: 'center',
   },
+  title: {
+     fontSize: 20,
+     marginBottom: 8,
+     textAlign: 'center',
+  },
+  year: {
+     textAlign: 'center',
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
